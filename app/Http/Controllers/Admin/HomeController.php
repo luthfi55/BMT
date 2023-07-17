ac549d03bc999baddcda724f8f00c7fcdf5eea80
luthfi55 <luthfi12354@gmail.com>
2023-07-17 11:45:03 +0700

    Add goods loan count dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Balance;
+use App\Models\GoodsLoan;
 use App\Models\LoanFund;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -15,8 +16,9 @@ class HomeController extends Controller
         $balance = Balance::first();
         $userCount = User::count();
         $loanFundCount = LoanFund::count();
+        $goodsLoanCount = GoodsLoan::count();
         // $goodLoanCount = GoodsLoan::count();
     
-        return view('admin.dashboard', compact('balance', 'userCount', 'loanFundCount'));
+        return view('admin.dashboard', compact('balance', 'userCount', 'loanFundCount', 'goodsLoanCount'));
     }
 }
